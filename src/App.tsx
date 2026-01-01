308f00097912e0b78c66d5732621c50bed610652
purenrg4life <nick.gray.purenrg@gmail.com>
2026-01-01 17:28:45 +0100

    Unify checkout success routing to CheckoutSuccess

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import CheckoutSuccess from "./pages/CheckoutSuccess";
 import Events from "./pages/Events";
 import Shop from "./pages/Shop";
 import PrivateEvents from "./pages/PrivateEvents";
@@ -13,37 +14,32 @@ import About from "./pages/About";
 import FAQ from "./pages/FAQ";
 import SantaPaddle from "./pages/SantaPaddle";
 import SonoranEchoes from "./pages/SonoranEchoes";
-import AlbumSuccess from "./pages/AlbumSuccess";
-import TrackSuccess from "./pages/TrackSuccess";
 import Analytics from "./pages/Analytics";
 import Admin from "./pages/Admin";
 import EventDetail from "./pages/EventDetail";
-import BookingSuccess from "./pages/BookingSuccess";
 import NotFound from "./pages/NotFound";
+
 
 function Router() {
   return (
     <>
       <Header />
       <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/events" component={Events} />
-        <Route path="/events/:id" component={EventDetail} />
-        <Route path="/booking/success" component={BookingSuccess} />
-        <Route path="/shop" component={Shop} />
-        <Route path="/sonoran-echoes" component={SonoranEchoes} />
-        <Route path="/album-success" component={AlbumSuccess} />
-        <Route path="/track-success" component={TrackSuccess} />
-        <Route path="/private-events" component={PrivateEvents} />
-        <Route path="/about" component={About} />
-        <Route path="/faq" component={FAQ} />
-        <Route path="/santa-paddle" component={SantaPaddle} />
-        <Route path="/analytics" component={Analytics} />
-        <Route path="/admin" component={Admin} />
-        <Route path="/404" component={NotFound} />
-        {/* Final fallback route */}
-        <Route component={NotFound} />
-      </Switch>
+  <Route path="/" component={Home} />
+  <Route path="/events" component={Events} />
+  <Route path="/events/:id" component={EventDetail} />
+  <Route path="/shop" component={Shop} />
+  <Route path="/sonoran-echoes" component={SonoranEchoes} />
+  <Route path="/private-events" component={PrivateEvents} />
+  <Route path="/about" component={About} />
+  <Route path="/faq" component={FAQ} />
+  <Route path="/santa-paddle" component={SantaPaddle} />
+  <Route path="/analytics" component={Analytics} />
+  <Route path="/admin" component={Admin} />
+  <Route path="/success" component={CheckoutSuccess} />
+  <Route path="/404" component={NotFound} />
+  <Route component={NotFound} />
+</Switch>
       <Footer />
     </>
   );
